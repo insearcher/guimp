8e2a9818b574ddcf091b29bff166fc579de877e9
Seeker Bednar <mpfrolovstud@gmail.com>
2019-04-04 02:25:43 +0300

    [CHANGE] bfs_iter.c

--- libui/src/bfs/bfs_iter.c
+++ libui/src/bfs/bfs_iter.c
@@ -3,35 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   bfs_iter.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edraugr- <edraugr-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sbednar <sbednar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/28 12:15:05 by edraugr-          #+#    #+#             */
-/*   Updated: 2019/03/28 13:50:20 by edraugr-         ###   ########.fr       */
+/*   Updated: 2019/04/04 02:25:23 by sbednar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libui.h"
 
-static void	draw_placeholder(void *el)
-{
-	char	*tmp;
-
-	tmp = ft_itoa(CAST_X_TO_Y(tmp, t_ui_el *)->test);
-	ft_putendl(tmp);
-	free(tmp);
-}
-
-void		bfs_iter(const t_list *root, void(*f)(const void *arg))
+void	bfs_iter(const t_list *root, void(*f)(const void *arg))
 {
 	QUEUE	*q;
 	void	*tmp;
 
 	q = NULL;
-	q_push(&q, root);
-	while (*q)
+	q_push(&q, (t_list *)root);
+	while (q)
 	{
-		tmp = q_pull(&q);
+		tmp = q_pop(&q);
 		q_push(&q, CAST_X_TO_Y(tmp, t_ui_el *)->children);
-		f(tmp);
+		if (f)
+			f(tmp);
 	}
 }
+
+void	bfs_iter_root(const t_ui_el *root, void(*f)(const void *arg))
+{
+	t_list *lst;
+
+	lst = ft_lstnew(NULL, 0);
+	lst->content = CAST_X_TO_Y(root, void *);
+	bfs_iter((const t_list *)lst, f);
+	free(lst);
+}
+

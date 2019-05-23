d2db066d3dcf62f77b215bc7ff3bdd86ae29083b
dolovnyak <dolovnyak@yandex.ru>
2019-05-23 06:03:28 +0300

    parents cut childrens (muslims)

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: sbednar <sbednar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/07 16:09:10 by sbednar           #+#    #+#             */
-/*   Updated: 2019/05/23 03:43:41 by sbecker          ###   ########.fr       */
+/*   Updated: 2019/05/23 05:50:04 by sbecker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -97,32 +97,6 @@ int		main(int argc, char *argv[])
 	el1.sdl_renderer = w.sdl_renderer;
 	ui_el_add_texture_from_file(&el1, "images/test2.jpg", TID_DEFAULT);
 	ui_el_setup_default_draggable(&el1);
-
-	t_ui_el el111;
-	ui_el_init(&el111);
-	ui_el_setup_default(&el111);
-	ui_event_add_listener(&(el111.events.onRender), &ui_el_draw_event);
-	ui_el_add_child(&(w.canvas), &el111);
-	ui_el_set_abs_pos(&el111, 50, 100);
-	ui_el_set_abs_size(&el111, 200, 200);
-	el111.id = 111;
-	el111.sdl_renderer = w.sdl_renderer;
-	ui_el_add_texture_from_file(&el111, "images/frolushka.jpeg", TID_DEFAULT);
-	ui_el_setup_default_draggable(&el111);
-
-	t_ui_el el100;
-	ui_el_init(&el100);
-	ui_el_setup_default(&el100);
-	ui_event_add_listener(&(el100.events.onRender), &ui_el_draw_event);
-	ui_el_add_child(&(el111), &el100);
-	ui_el_set_abs_pos(&el100, 50, 400);
-	ui_el_set_abs_size(&el100, 150, 150);
-	el100.params |= EL_IS_DEPENDENT;
-	el100.id = 1000;
-//	el100.params |= EL_IGNOR_RAYCAST;
-	el100.sdl_renderer = w.sdl_renderer;
-	ui_el_add_texture_from_file(&el100, "images/prison.jpg", TID_DEFAULT);
-	ui_el_setup_default_draggable(&el100);
 
 	t_ui_el el2;
 	ui_el_init(&el2);

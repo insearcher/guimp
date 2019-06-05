cae4fd09e99480c670f02e4bb063ce15cbee6bf8
dolovnyak <dolovnyak@yandex.ru>
2019-06-05 19:47:05 +0300

    fix slider

--- libui/src/ui_el/ui_el_drag.c
+++ libui/src/ui_el/ui_el_drag.c
@@ -6,7 +6,7 @@
 /*   By: edraugr- <edraugr-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/25 20:15:41 by sbednar           #+#    #+#             */
-/*   Updated: 2019/06/05 17:20:56 by edraugr-         ###   ########.fr       */
+/*   Updated: 2019/06/05 19:46:28 by sbecker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,10 +39,11 @@ void	ui_el_hor_slider_drug(void *a1, void *a2)
 
 	m = (t_ui_main *)a1;
 	el = (t_ui_el *)a2;
+	printf("1: (%d, %d)\n", el->rect.x, el->rect.y);
 	x = m->ptr_pos.x - el->ptr_rel_pos.x;
 	y = el->rect.y;
 	if (x < el->parent->rect.x ||
 		x + el->rect.w > el->parent->rect.x + el->parent->rect.w)
 		return ;
-	ui_el_set_new_pos(el, el->parent, PIXEL, (t_fvec2){-x, y});
+	ui_el_set_new_pos(el, 0, ABS | PIXEL, (t_fvec2){x, y});
 }

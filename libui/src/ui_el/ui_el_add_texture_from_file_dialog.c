9be35e8dc533f8c7b50ea6e3d3c83388b70f1a6a
Seeker Bednar <mpfrolovstud@gmail.com>
2019-06-09 23:53:32 +0300

    Added function to read texture from file

--- libui/src/ui_el/ui_el_add_texture_from_file_dialog.c
+++ libui/src/ui_el/ui_el_add_texture_from_file_dialog.c
@@ -6,7 +6,7 @@
 /*   By: sbednar <sbednar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/09 23:16:17 by sbednar           #+#    #+#             */
-/*   Updated: 2019/06/09 23:19:30 by sbednar          ###   ########.fr       */
+/*   Updated: 2019/06/09 23:53:20 by sbednar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,30 @@ int	ui_el_add_texture_from_file_dialog(t_ui_el *el)
 	if (ui_open_file_dialog(&path) < 0)
 		return (FUNCTION_FAILURE);
 	ui_el_add_texture_from_file(el, path, "default");
+	free(path);
 	return (FUNCTION_SUCCESS);
+}
+
+int	ui_el_add_texture_from_file_dialog_with_size(t_ui_el *el, int w, int h)
+{
+	char		*path;
+	SDL_Texture	*tmp_src;
+	SDL_Texture	*tmp;
+	t_list		*tmp_lst;
+
+	path = NULL;
+	if (ui_open_file_dialog(&path) < 0 || !(tmp_lst = ft_lstnew(NULL, 0)) ||
+		!(tmp = SDL_CreateTexture(el->sdl_renderer, SDL_PIXELFORMAT_RGBA8888,
+		SDL_TEXTUREACCESS_TARGET, w, h)) ||
+		ui_el_load_surface_from(el, path) < 0 ||
+		!(tmp_src = ui_el_create_texture(el)))
+		return (FUNCTION_FAILURE);
+	SDL_SetRenderTarget(el->sdl_renderer, tmp);
+	SDL_RenderCopy(el->sdl_renderer, tmp_src, NULL, NULL);
+	SDL_SetRenderTarget(el->sdl_renderer, NULL);
+	free(path);
+	tmp_lst->content_size = ft_strhash("default");
+	tmp_lst->content = (void *)tmp;
+	ft_lstadd(&(el->sdl_textures), tmp_lst);
+	return(FUNCTION_SUCCESS);
 }

94ba69f604b1079b43f9c8778a275c9dc2b479a3
Etienne Draugr death overlord <edraugr-@at-p1.21-school.ru>
2019-06-19 22:24:52 +0300

    mergeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- include/guimp.h
+++ include/guimp.h
@@ -6,7 +6,7 @@
 /*   By: edraugr- <edraugr-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/10 19:04:10 by sbednar           #+#    #+#             */
-/*   Updated: 2019/06/10 00:07:39 by edraugr-         ###   ########.fr       */
+/*   Updated: 2019/06/19 22:03:35 by edraugr-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,14 +71,17 @@
 # define GM_TOOL_LINE			4
 
 # define GM_TOOL_STATE_NONE		0
-# define GM_TOOL_STATE_DRAW		1
-# define GM_TOOL_STATE_END		2
+# define GM_TOOL_STATE_START	1
+# define GM_TOOL_STATE_DRAW		2
+# define GM_TOOL_STATE_END		3
 
 //id_generator flags
 # define ID_GENERATOR_GET		0
 # define ID_GENERATOR_ADD		1
 # define ID_GENERATOR_DEL		2
 # define ID_GENERATOR_START		63
+
+# define DEBUG_STATUS			0
 
 /* t_layers - struct for elems in layer win */
 typedef struct					s_layers

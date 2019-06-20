05547dab82399fa728292d692f2ab8b0fcd9b6cf
Seeker Bednar <mpfrolovstud@gmail.com>
2019-06-20 20:23:45 +0300

    Added nice logs for json parsing

--- libui/src/ui_jtoc/ui_jtoc_utils.c
+++ libui/src/ui_jtoc/ui_jtoc_utils.c
@@ -6,7 +6,7 @@
 /*   By: sbednar <sbednar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/19 20:05:37 by sbednar           #+#    #+#             */
-/*   Updated: 2019/06/20 20:10:59 by sbednar          ###   ########.fr       */
+/*   Updated: 2019/06/20 20:23:41 by sbednar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 int	ui_sdl_log_error(const char *p, const int id)
 {
 	SDL_Log("%s ----> ERROR <---- %s", KRED, KNRM);
-	SDL_Log("INCORRECT: %s%s%s",
+	SDL_Log("INCORRECT: %s%s%s%s%s",
 		p,
 		id < 0 ? "" : " IN ID = ",
-		id < 0 ? "" : ft_itoa(id)
+		KGRN,
+		id < 0 ? "" : ft_itoa(id),
+		KNRM
 	);
 	return (FUNCTION_FAILURE);
 }
